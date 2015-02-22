bb7133c9a95cc539279bb2ec88af8ea61d51c61e
Michael Dobekidis <m.dobekidis@7linternational.com>
2015-02-22 10:18:06 +0200

    added read-me notes Added main script

--- README.md
+++ README.md
@@ -1,2 +1,36 @@
 # phaser_modals
 Phaser.io Modals creator script
+
+<strong>General Options:</strong>
+
+<ul>
+	<li><strong>type:</strong> This is the name of the modal, which we use to identify it</li>
+	<li><strong>includeBackground:</strong> This toggles the modal background on or off (default off)</li>
+        <li><strong>backgroundColor:</strong> This is the modal background color (default 0x000000)</li>
+	<li><strong>backgroundOpacity: </strong> This sets the modal background opacity (default: 0.7)</li>
+	<li><strong>modalCloseOnInput:</strong> If we want the modal to close when we click on the background (default off)</li>
+</ul>
+
+<strong>Modal elements properties</strong>
+
+<ul>
+	<li><strong>type</strong> | The type of the element (text, bitmapText, image)</li>
+	<li><strong>color</strong> | The color of the element (text only)</li>
+	<li><strong>fontFamily</strong> | The font family to use (bitmapText or text)</li>
+	<li><strong>fontSize</strong> | The font size of the text or bitmaptext (bitmapText or text)</li>
+	<li><strong>align</strong> | The alignment of the text (center, left, right and text only)</li>
+	<li><strong>offsetX</strong> | The offset on x-axis of the element from the center of the game (0 is dead center)</li>	<li><strong>offsetY</strong> | The offset on y-axis of the element from the center of the game (0 is dead center)</li>	<li><strong>contentScale</strong> | The scaling to apply to the element (1 is no scale, default = 1)</li>
+	<li><strong>content</strong> | The actual content of the element can be string, number, cache key (for images), url of image</li>
+	<li><strong>callback</strong> | The callback function to call when the element is clicked/touched (basically when input down event is fired)</li>
+</ul>
+
+<strong>Available functions</strong>
+
+<ul>
+	<li><strong>createModal</strong> | The function to create a modal, takes an object with the modal properties as an argument</li>
+	<li><strong>showModal</strong> | The function to show a previously created modal, takes the modal name/type as an argument</li>
+	<li><strong>hideModal</strong> | The function to hide a previously created and shown modal, takes the modal name/type as an argument</li>
+	<li><strong>getModalItem</strong> | The function to get an element from a previously created modal, takes the modal name/type and the position of the element (index) as an argument</li>
+	<li><strong>destroyModal</strong> | Remove a previously created modal from the registry</li>
+	<li><strong>updateModalValue</strong> | Dynamically change the value of an element inside a modal, takes the modal name/type, the changed value, and the index of the element as arguments (experimental function)</li>
+</ul>
---
+++ src/modal.js
@@ -0,0 +1,288 @@
+var gameModal = gameModal || {};
+
+
+gameModal = function (game) {
+
+    var _this = this;
+
+    game.modals = {};
+
+    /**
+     * [hideModal description]
+     * @param  {[type]} type [description]
+     * @return {[type]}      [description]
+     */
+    this.hideModal = function(type) {
+        window.console.log(type);
+        game.modals[type].visible = false;
+    };
+
+    return {
+
+        createModal: function (options) {
+
+            var type = options.type || ''; // must be unique
+            var includeBackground = options.includeBackground; // maybe not optional
+            var backgroundColor = options.backgroundColor || "0x000000";
+            var backgroundOpacity = options.backgroundOpacity || 0.7;
+            var modalCloseOnInput = options.modalCloseOnInput || false;
+            var vCenter = options.vCenter || true;
+            var hCenter = options.hCenter || true;
+            var itemsArr = options.itemsArr || [];
+            /*var vPadding = options.vPadding || 20;*/
+
+            /////////////////////////////////////////////////////////////////////
+
+            var modal;
+            var modalGroup = game.add.group();
+            modalGroup.inputEnabled = true;
+
+            if (includeBackground === true) {
+                modal = game.add.graphics(game.width, game.height);
+                modal.beginFill(backgroundColor, backgroundOpacity);
+                modal.x = 0;
+                modal.y = 0;
+                modal.inputEnabled = true;
+                modal.drawRect(0, 0, game.width, game.height);
+
+                if(modalCloseOnInput === true) {
+                    var innerModal = game.add.sprite(0, 0);
+                    innerModal.inputEnabled = true;
+                    innerModal.width = game.width;
+                    innerModal.height = game.height;
+                    innerModal.type = type;
+                    innerModal.events.onInputDown.add(function(e){
+                        window.console.log(this, e.type);
+                        this.hideModal(e.type);
+                    }, _this);
+
+                    modalGroup.add(innerModal);
+                }
+            }
+
+            // add the bg
+            if(includeBackground) {
+                modalGroup.add(modal);
+            }
+
+            var modalLabel;
+            for (var i = 0; i < itemsArr.length; i += 1) {
+                var item = itemsArr[i];
+                var itemType = item.type || 'text';
+                var itemColor = item.color || 0x000000;
+                var itemFontfamily = item.fontFamily || 'Arial';
+                var itemFontSize = item.fontSize || 32;
+                var itemStroke = item.stroke || '';
+                var itemAlign = item.align || 'center';
+                var offsetX = item.offsetX || 0;
+                var offsetY = item.offsetY || 0;
+                var contentScale = item.contentScale || 1;
+                var content = item.content || "";
+                var centerX = game.width / 2;
+                var centerY = game.height / 2;
+                var callback = item.callback || false;
+
+                modalLabel = null;
+
+                if (itemType === "text" || itemType === "bitmapText") {
+
+                    if (itemType === "text") {
+                        modalLabel = game.add.text(0, 0, content, {
+                            font: itemFontSize + 'px ' + itemFontfamily,
+                            fill: "#" + String(itemColor).replace("0x",""),
+                            align: itemAlign
+                        });
+                        modalLabel.contentType = 'text';
+                        modalLabel.update();
+                        modalLabel.x = ((game.width / 2) - (modalLabel.width / 2)) + offsetX;
+                        modalLabel.y = ((game.height / 2) - (modalLabel.height / 2)) + offsetY;
+                    } else {
+                        modalLabel = game.add.bitmapText(0, 0, itemFontfamily, String(content), itemFontSize);
+                        modalLabel.contentType = 'bitmapText';
+                        modalLabel.updateText();
+                        modalLabel.x = (centerX - (modalLabel.width / 2)) + offsetX;
+                        modalLabel.y = (centerY - (modalLabel.height / 2)) + offsetY;
+                    }
+
+                } else if (itemType === "image") {
+                    //content = item.imageKey || "";
+                    modalLabel = game.add.image(0, 0, content);
+                    modalLabel.scale.setTo(contentScale, contentScale);
+                    modalLabel.contentType = 'image';
+                    modalLabel.x = (centerX - ((modalLabel.width) / 2)) + offsetX;
+                    modalLabel.y = (centerY - ((modalLabel.height) / 2)) + offsetY;
+                }
+
+                modalLabel.offsetX = offsetX;
+                modalLabel.offsetY = offsetY;
+
+                if (callback !== false) {
+                    modalLabel.inputEnabled = true;
+                    modalLabel.events.onInputDown.add(callback, this);
+                }
+
+                modalGroup.add(modalLabel);
+            }
+
+            modalGroup.visible = false;
+            game.modals[type] = modalGroup;
+
+        },
+        updateModalValue: function(value, type, index, id) {
+            var item;
+            if(index !== undefined && index !== null) {
+                item = game.modals[type].getChildAt(index);
+            }
+            else if(id !== undefined && id !== null) {
+
+            }
+
+            if(item.contentType === "text") {
+                item.text = value;
+                item.update();
+                item.x = ((game.width / 2) - (item.width / 2)) + item.offsetX;
+                item.y = ((game.height / 2) - (item.height / 2)) + item.offsetY;
+            }
+            else if(item.contentType === "bitmapText") {
+                item.text = value;
+                item.updateText();
+                item.x = ((game.width / 2) - (item.width / 2)) + item.offsetX;
+                item.y = ((game.height / 2) - (item.height / 2)) + item.offsetY;
+            }
+            else if(item.contentType === "image") {
+                item.loadTexture(value);
+            }
+
+        },
+        getModalItem: function(type, index) {
+            return game.modals[type].getChildAt(index);
+        },
+        showModal: function (type) {
+            game.world.bringToTop(game.modals[type]);
+            game.modals[type].visible = true;
+            // you can add animation here
+        },
+        hideModal: function (type) {
+            game.modals[type].visible = false;
+            // you can add animation here
+        },
+        destroyModal: function (type) {
+            game.modals[type].destroy();
+            delete game.modals[type];
+        }
+    };
+};
+
+
+/**
+ * [createModal description]
+ * @param  {[type]} type [description]
+ * @return {[type]}      [description]
+ */
+function createModal(type, fn) {
+
+    var modalGroup = game.add.group();
+    modalGroup.inputEnabled = true;
+
+    var modal = game.add.graphics(game.width, game.height);
+    modal.beginFill("0xffffff", 0.7);
+    modal.x = 0;
+    modal.y = 0;
+    modal.inputEnabled = true;
+    modal.drawRect(0, 0, game.width, game.height);
+
+    //var modalPanel = game.add.image((game.width / 2 - (550 / 2)), (game.height / 2 - (400 / 2)), "modal");
+
+    if (type === "game-over") {
+
+        var gameLabel = game.add.image(game.width / 2 - ((358 * 0.6) / 2), (game.height / 2) - 250, 'gameover');
+        gameLabel.scale.setTo(0.6, 0.6);
+        var gameLabel1 = game.add.image(game.width / 2 - ((533 * 0.6) / 2), game.height / 2, 'tryagain');
+        gameLabel1.scale.setTo(0.6, 0.6);
+        var yesLabel = game.add.image(game.width / 2 - 200, game.height / 2 + 150, 'yes');
+        var noLabel = game.add.image(game.width / 2 + 80, game.height / 2 + 150, 'no');
+
+        yesLabel.scale.setTo(0.8, 0.8);
+        noLabel.scale.setTo(0.8, 0.8);
+
+        // -----------
+
+        yesLabel.inputEnabled = true;
+        yesLabel.events.onInputDown.add(function () {
+            game.state.start('Game');
+        }, this);
+
+        noLabel.inputEnabled = true;
+        noLabel.events.onInputDown.add(function () {
+            game.state.start('MainMenu');
+        }, this);
+
+        modalGroup.add(modal);
+        modalGroup.add(gameLabel);
+        modalGroup.add(gameLabel1);
+        modalGroup.add(yesLabel);
+        modalGroup.add(noLabel);
+
+    } else if (type === "level") {
+
+        var modalText = game.add.bitmapText(0, 0, 'font', "Starting \nNext Level", 52);
+
+        modalText.inputEnabled = true;
+        modalText.updateText();
+        modalText.x = game.width / 2 - (modalText.width / 2);
+        modalText.y = game.height / 2 - (modalText.height / 2) - 200;
+
+        // ------------
+
+        var countDownText = game.add.bitmapText(0, 0, 'font', "3", 38);
+
+        //countDownText.inputEnabled = true;
+        countDownText.updateText();
+        countDownText.x = game.width / 2 - (countDownText.width / 2);
+        countDownText.y = game.height / 2 - (countDownText.height / 2) - 100;
+
+        reg.countDownText = countDownText;
+
+        modal.destroy();
+        //modalGroup.add(modal);
+        modalGroup.add(modalText);
+        modalGroup.add(countDownText);
+
+    } else if (type === "info") {
+
+        // -----------
+        var modalText = game.add.bitmapText(0, 0, 'font', "Click/TAP the signs \nin the correct order", 32);
+
+        modalText.inputEnabled = true;
+        modalText.updateText();
+        modalText.x = game.width / 2 - (modalText.width / 2);
+        modalText.y = game.height / 2 - (modalText.height / 2);
+
+        var closeX = game.add.bitmapText(0, 0, 'font', 'Got it!', 40);
+        closeX.inputEnabled = true;
+        closeX.updateText();
+        closeX.x = game.width / 2 - (closeX.width / 2);
+        closeX.y = game.height - 300;
+
+
+        closeX.events.onInputDown.add(function () {
+            fn();
+            hideModal('info');
+        }, game);
+
+        modalGroup.add(modal);
+        modalGroup.add(closeX);
+        //modalGroup.add(modalPanel);
+        //modalGroup.add(modalHeader);
+        //modalGroup.add(gameLabel2);
+        modalGroup.add(modalText);
+    }
+
+    modalGroup.visible = false;
+
+    reg.modal[type] = modalGroup;
+
+    if (type === "info") {
+        showModal(type);
+    }
+}
